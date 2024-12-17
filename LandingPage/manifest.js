29fad6b294e3ad6776cd3d6c44c1b8bd409296b8
egifn_ <egifajar.n@gmail.com>
2024-12-17 15:40:08 +0700

    Update

---
+++ LandingPage/manifest.js
@@ -0,0 +1,20 @@
+{
+    "name": "OOTD",
+    "short_name": "OOTD",
+    "start_url": "/LandingPage/SizeChart.html",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#007bff",
+    "icons": [
+        {
+            "src": "/img/logo.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/img/logo.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
+}
